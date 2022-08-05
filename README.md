e6a7bae31593504df2c1fb833e5c8b694f624ff1
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2022-08-05 10:07:44 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_postman
+
+Flutter Postman
---
+++ lib/ui/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const Text('Hello'));
+  }
+}
